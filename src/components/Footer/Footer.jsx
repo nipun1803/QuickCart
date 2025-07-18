340efe239da99e400716c23f19a9cbdbfae2ed4b
nipun1803 <nipun2006134@gmail.com>
2025-07-18 11:23:38 +0530

    added filter option

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import { Link } from 'react-router-dom';
+import './Footer.css';
+export const Footer = () => {
+  return (
+    <footer className='main-footer'>
+      <div className='footer-links'>
+        <a
+          target={'_blank'}
+          href='https://github.com/nipun1803'
+          className='fab fa-github'
+        ></a>
+        <a
+          target={'_blank'}
+          href='https://www.linkedin.com/in/nipun-patlori-2a5bb830a/'
+          className='fab fa-linkedin'
+        ></a>
+      </div>
+      <div className='footer-text'>© {new Date().getFullYear()} Quickcart. All rights reserved.</div>
+    </footer>
+  );
+};
