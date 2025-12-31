d408d4314c5a3e82e0069c36aa96edde80614acf
Nipun Reddy <nipun2006134@gmail.com>
2025-12-31 22:09:06 +0530

    deploy commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
-import 'dotenv/config'; // Load env vars immediately
+import 'dotenv/config'; 
 import express from 'express';
-// import dotenv from 'dotenv'; // No longer needed as we use 'dotenv/config'
 import cors from 'cors';
-import cookieParser from 'cookie-parser'; // Import cookie-parser
-import session from 'express-session'; // Import session
+import cookieParser from 'cookie-parser'; 
+import session from 'express-session';
 import connectDB from './config/db.js';
 import './config/passport.js';
 import passport from 'passport';
 import errorHandler from './middleware/errorHandler.js';
-
-// Import routes
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cartRoutes from './routes/cartRoutes.js';
@@ -18,17 +15,15 @@ import adminRoutes from './routes/adminRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import wishlistRoutes from './routes/wishlistRoutes.js';
 
-// Load environment variables
-// dotenv.config(); // Loaded via import 'dotenv/config'
 
-// Connect to MongoDB
+
 connectDB();
 
 const app = express();
 
-// Middleware
+
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://localhost:5174'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
     credentials: true,
 }));
 app.use(express.json());
