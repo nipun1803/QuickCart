df5cf2e37f9b3e4225fba66d3111e379233701dd
Nipun Reddy <nipun2006134@gmail.com>
2026-03-19 23:28:41 +0530

    fixed cors error by adding new origing for e2e

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 const app = express();
 app.set('trust proxy', 1);
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL, 'http://localhost:5173','http://127.0.0.1:5173'],
     credentials: true,
 }));
 
